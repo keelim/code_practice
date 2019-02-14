700b85eef9ace4d12e517003879f4b3fdf9cb5ef
Keel_im <kimh00335@gmail.com>
2019-02-14 23:40:45 +0900

    2019 02 14

--- src/main/java/Baek_11720.java
+++ src/main/java/Baek_11720.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Baek_11720 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int anInt = sc.nextInt();
-        sc.nextLine();
-        String number = sc.nextLine();
-        int[] ints = new int[anInt];
-        int answer = 0;
-        for (int i = 0; i < number.length(); i++) {
-            answer += Integer.parseInt(number.substring(i, i + 1));
-        }
-        System.out.println(answer);
+//        Scanner sc = new Scanner(System.in);
+//        int anInt = sc.nextInt();
+//        sc.nextLine();
+//        String number = sc.nextLine();
+//        int[] ints = new int[anInt];
+//        int answer = 0;
+//        for (int i = 0; i < number.length(); i++) {
+//            answer += Integer.parseInt(number.substring(i, i + 1));
+//        }
+//        System.out.println(answer);
+
+        String [] a = new String[3];
+
+        System.out.println(a.length);
 
     }
 
---
+++ src/main/java/baek_10989.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        ArrayList<String> list = new ArrayList();
+
+        int count, nums = 0;
+        count = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < count; i++) {
+            nums = Integer.parseInt(reader.readLine());
+            list.add(String.valueOf(nums));
+        }
+        Collections.sort(list);
+        for (int i = 0; i <list.size(); i++) {
+            writer.write(list.get(i));
+        }
+        writer.flush();
+
+    }
+}
