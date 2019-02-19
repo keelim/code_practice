632ff3d43feef3d5edfdd8129885a27a9d9fb930
Keel_im <kimh00335@gmail.com>
2019-02-20 00:14:53 +0900

    2019 02 20

--- src/main/java/baek_10989.java
+++ src/main/java/baek_10989.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class baek_10989 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,6 +19,7 @@ public class Main {
             writer.write(list.get(i));
         }
         writer.flush();
+        String a = "3";
 
     }
 }
