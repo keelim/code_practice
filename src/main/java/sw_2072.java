0a953e36c2290c587d6b3f8b9b63804f8adbe852
Keel_im <kimh00335@gmail.com>
2019-02-21 23:43:02 +0900

    Create sw_2072.java

---
+++ src/main/java/sw_2072.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class sw_2072 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int test_case = Integer.parseInt(sc.nextLine());
+        for (int t = 1; t <= test_case; t++) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            String[] temp = sc.nextLine().split(" ");
+            for (String s : temp) {
+                int change_s = Integer.parseInt(s);
+                if (change_s % 2 != 0) {
+                    arrayList.add((change_s));
+                }
+
+            }
+            int odd_sum=0;
+            for ( int a: arrayList){
+                odd_sum+=a;
+            }
+            System.out.println("#"+t+" " + odd_sum);
+
+        }
+
+
+    }
+
+}
