97af7681732303a420f864cd6cf25296bcd811aa
Keel_im <kimh00335@gmail.com>
2019-02-24 17:23:08 +0900

    2019 02 24

---
+++ src/main/java/S/S_2058.java
@@ -0,0 +1,28 @@
+package S;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class S_2058 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] temp = sc.nextLine().split("");
+        ArrayList<Integer> list = new ArrayList<>();
+        for (String a : temp)
+            list.add(Integer.parseInt(a));
+        int answer = 0;
+
+        for (int a : list)
+            answer+=a;
+
+        System.out.println(answer);
+
+    }
+
+
+}
+
+
