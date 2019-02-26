04351a25915a1c1be45f0d04cbcbeb2d9145f661
Keel_im <kimh00335@gmail.com>
2019-02-26 21:02:34 +0900

    Create S_2019.java

---
+++ src/main/java/S/S_2019.java
@@ -0,0 +1,19 @@
+package S;
+
+import java.util.Scanner;
+
+public class S_2019 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int answer;
+        int anInt = Integer.parseInt(sc.nextLine());
+        for (int t = 0; t <= anInt; t++) {
+            answer = (int) Math.pow(2, t);
+            System.out.print(answer + " ");
+        }
+
+    }
+
+}
