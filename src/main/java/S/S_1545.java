084c8a66a98c73b8043c488d415e4d229c01a0d5
Keel_im <kimh00335@gmail.com>
2019-02-26 20:51:10 +0900

    Create S_1545.java

---
+++ src/main/java/S/S_1545.java
@@ -0,0 +1,19 @@
+package S;
+
+import java.util.Scanner;
+
+public class S_1545 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test_case = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i <=test_case; i++) {
+            System.out.print(test_case-i + " ");
+        }
+
+
+    }
+
+
+}
+
+
