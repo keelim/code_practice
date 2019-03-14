c972e57db963965851787576426619aba1f9743f
Keel_im <kimh00335@gmail.com>
2019-03-14 21:41:15 +0900

    Create S_1959.java

---
+++ src/main/java/S/D2/S_1959.java
@@ -0,0 +1,23 @@
+package S.D2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class S_1959 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test_case = Integer.parseInt(sc.nextLine());
+
+        for (int t = 1; t <= test_case ; t++) {
+            ArrayList<Integer> arrayList1 = new ArrayList<>();
+            ArrayList<Integer> arrayList2 = new ArrayList<>();
+
+
+            for(String a : sc.nextLine().split(" "))
+
+        }
+
+
+    }
+
+}
