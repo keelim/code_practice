c116495e94119077aa0ded6beac518b5a8536a4d
Keel_im <kimh00335@gmail.com>
2019-04-22 15:26:18 +0900

    Recursion complete

--- src/main/java/practice_code/Recursion.java
+++ src/main/java/practice_code/Recursion.java
@@ -68,4 +68,43 @@ public class Recursion {
             }
         }
     }
+
+    public void quickSort(int[] a, int left, int right) {
+        quickRecursively(a, left, right);
+    }
+
+    private void quickRecursively(int[] a, int left, int right) {
+        if (left < right) {
+            int mid = partition(a, left, right);
+            quickRecursively(a, left, mid - 1);
+            quickRecursively(a, mid + 1, right);
+        }
+
+    }
+
+    private int partition(int[] a, int left, int right) {
+        int pivot = left;
+        int toRight = left;
+        int toLeft = right + 1;
+        do {
+            do {
+                toRight++;
+            } while (a[toRight] < a[pivot]);
+            do {
+                toLeft--;
+            } while (a[toLeft] > a[pivot]);
+            if (toRight < toLeft) {
+                swap(a, toLeft, toRight);
+            }
+
+        } while (toRight < toLeft);
+        swap(a, left, toLeft);
+        return toLeft;
+    }
+
+    public void swap(int[] a, int left, int right) {
+        int temp = a[left];
+        a[left] = a[right];
+        a[right] = temp;
+    }
 }
