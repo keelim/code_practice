4f90bcff0a5f3af979dc1ef367c856c49ad207b1
keelim <kimh00335@gmail.com>
2019-10-31 23:36:39 +0900

    2019 10 09

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,17 @@
-import java.util.Base64;
 import java.util.Scanner;
 
 public class Solution {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int testCase = Integer.parseInt(sc.nextLine().trim());
-        for (int i = 1; i <= testCase; i++) {
-            String answer = sc.nextLine();
-            answer = new String(Base64.getDecoder().decode(answer.getBytes()));
-            System.out.println("#" + i + " " + answer);
+        int testCase = sc.nextInt();
+        for (int i = 1; i <= testCase ; i++) {
+            int answer = 0;
+            for (int j = 0; j < 10; j++) {
+                answer+= sc.nextInt();
+            }
+            answer/=10;
+            System.out.println("#"+i+" " + answer);
         }
     }
 }
