03617e648c1eb50d81d2a5586df9c5b9780cbff8
keelim <kimh00335@gmail.com>
2019-11-05 23:13:40 +0900

    2019 11 05

--- src/Solution.java
+++ src/Solution.java
@@ -4,14 +4,20 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int testCase = sc.nextInt();
-        for (int i = 1; i <= testCase ; i++) {
-            int answer = 0;
-            for (int j = 0; j < 10; j++) {
-                answer+= sc.nextInt();
-            }
-            answer/=10;
-            System.out.println("#"+i+" " + answer);
+        for (int i = 1; i <= testCase; i++) {
+            String answer;
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (a == b)
+                answer = "=";
+            else if (a > b)
+                answer = ">";
+            else
+                answer = "<";
+
+            System.out.println("#" + i + " " + answer);
         }
     }
 }
