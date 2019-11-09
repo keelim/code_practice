de724a16d9cf1efcc6f538014f1a4dd417c815bf
keelim <kimh00335@gmail.com>
2019-11-09 17:50:35 +0900

    2019 11 09

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,41 +1,6 @@
-#include <iostream>
-#include <string>
-
+#include<iostream>
 using namespace std;
 
-int main() {
-    int testCase;
-    cin >> testCase;
-    for (int i = 1; i <= testCase; i++) {
-        string s;
-        cin >> s;
-        if (s.length() != 8) {
-            cout << "#" << i << " " << -1 << "\n";
-            continue;
-        }
-
-        string year = s.substr(0, 4);
-        string month = s.substr(4, 2);
-        int iMonth = stoi(month);
-        string day = s.substr(7, 2);
-        int iDay = stoi(day);
-
-        int end[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};;
-        if (iMonth == 0 || iMonth > 12) {
-            cout << "#" << i << " " << -1 << "\n";
-            continue;
-        } else if (iDay == 0 || iDay > 31) {
-            cout << "#" << i << " " << -1 << "\n";
-            continue;
-        } else if (iDay > end[iMonth]) {
-            cout << "#" << i << " " << -1 << "\n";
-            continue;
-        }
-
-        cout << "#" << i << " " << year << "/" << month << "/" << day << "\n";
-
-
-    }
-
-
+int main () {
+	
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,38 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int testCase = sc.nextInt();
-        for (int i = 1; i <= testCase; i++) {
-            String temp = sc.next();
-            if (temp.length() != 8) {
-                System.out.println("#" + i + " " + -1);
-                continue;
-            }
 
-            String year = temp.substring(0, 4);
-            String month = temp.substring(4, 6);
-            int iMonth = Integer.parseInt(month);
-            String day = temp.substring(6);
-            int iDay = Integer.parseInt(day);
+        ArrayList<Character> list = new ArrayList<>();
+        String[] temp = sc.nextLine().split("");
 
+        for (int i = 0; i < temp.length; i++) {
+            list.add(temp[i].charAt(0));
+        }
 
-            int[] end = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-            if (iMonth == 0 || iMonth >= 13) {
-                System.out.println("#" + i + " " + -1);
-                continue;
-            } else if (iDay == 0 || iDay >= 32) {
-                System.out.println("#" + i + " " + -1);
-                continue;
-            } else if (iDay > end[iMonth]) {
-                System.out.println("#" + i + " " + -1);
-                continue;
-            }
-
-            System.out.println("#" + i + " " + year + "/" + month + "/" + day);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i)-64 + " ");
         }
     }
 }
+
+
