f507bf86486c95ae78ed730fb399867f0dd100ee
keelim <kimh00335@gmail.com>
2019-11-10 15:06:36 +0900

    2047

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String temp = sc.nextLine();
+        temp = temp.toUpperCase();
+        System.out.println(temp);
 
-        ArrayList<Character> list = new ArrayList<>();
-        String[] temp = sc.nextLine().split("");
-
-        for (int i = 0; i < temp.length; i++) {
-            list.add(temp[i].charAt(0));
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            System.out.print(list.get(i)-64 + " ");
-        }
     }
 }
 
