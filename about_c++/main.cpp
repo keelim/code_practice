50956cd8930caed450b962670835bde2239eec25
keelim <kimh00335@gmail.com>
2019-11-11 12:42:26 +0900

    2043

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main () {
-	
+	int testNum;
+	cin >> testNum;
+	for (auto i=0; i < testNum; i++) {
+		cout << "#";
+	}
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,11 +3,8 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String temp = sc.nextLine();
-        temp = temp.toUpperCase();
-        System.out.println(temp);
-
+        int pass = sc.nextInt(), input = sc.nextInt();
+        System.out.println(pass - input + 1);
     }
 }
-
 
