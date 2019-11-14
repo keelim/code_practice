fb7c85e8feb87a598f759c85e6f26340394b1747
keelim <kimh00335@gmail.com>
2019-11-14 16:17:13 +0900

    2019

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main () {
-	for (auto i=0; i < 5; i++) {
-		for (auto j=0; j < 5; j++) {
-			if (i == j)
-				cout << "#";
-			else
-				cout << "+";
-		}
+	int data;
+	cin >> data;
+	int answer=0;
+	for (auto i=0; i <= data; i++) {
+		answer+=i;
 	}
+	cout << answer;
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
-        int data = Integer.parseInt(sc.nextLine());
+        int data = sc.nextInt();
         int answer = 0;
-
-        for (int i = 0; i <= data; i++) {
-            answer+=i;
+        for (int i = 1; i <= data; i++) {
+            answer += i;
         }
         System.out.println(answer);
     }
