a304d5a15c66f385ac5f557cf0a0b5e0ea029721
keelim <kimh00335@gmail.com>
2019-11-14 16:23:31 +0900

    2019

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main () {
-	int data;
-	cin >> data;
-	int answer=0;
-	for (auto i=0; i <= data; i++) {
-		answer+=i;
+	int aint;
+	cin >> aint;
+	int answer=1;
+	for (auto i=0; i <= aint; i++) {
+		cout << answer << " ";
+		answer*=2;
 	}
-	cout << answer;
 }
