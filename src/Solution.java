dd82a8a8734a5eb230e1506309b6dbbdd19c7d0a
keelim <kimh00335@gmail.com>
2019-11-14 16:10:58 +0900

    2027

--- src/Solution.java
+++ src/Solution.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int test_case = sc.nextInt();
-
-        for (int t = 1; t <= test_case; t++) {
-            int first = sc.nextInt(), second = sc.nextInt();
-            System.out.println("#" + t +" "+ first / second + " " + first % second);
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == i)
+                    System.out.print("#");
+                else
+                    System.out.print("+");
+            }
+            System.out.println();
         }
     }
 }
