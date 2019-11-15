97120152c26bfbb3dec7c58b9a64783631dbf6ec
keelim <kimh00335@gmail.com>
2019-11-15 15:16:15 +0900

    1938

--- src/Solution.java
+++ src/Solution.java
@@ -4,11 +4,14 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int anInt = sc.nextInt();
-        int answer = 1;
-        for (int t = 0; t <= anInt; t++) {
-            System.out.print(answer + " ");
-            answer *= 2;
-        }
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+
+        System.out.println(first + second);
+        System.out.println(first - second);
+        System.out.println(first * second);
+        System.out.println(first / second);
     }
+
+
 }
