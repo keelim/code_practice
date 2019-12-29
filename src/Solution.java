b2abc55cba233f0cc7ab9cd2d7f7d8388204146a
keelim <kimh00335@gmail.com>
2019-12-29 22:37:27 +0900

    2019 12 29

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,21 @@
 import java.util.Scanner;
-import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int testCase = sc.nextInt();
+        for (int i = 1; i <= testCase; i++) {
+            String answer = "";
+            String temp = sc.next();
+            temp = temp.substring(temp.length()-1, temp.length());
+            int now = Integer.parseInt(temp);
+            if(now%2==0)
+                answer="Even";
+            else
+                answer="Odd";
 
-        for (int i = 0; i < testCase; i++) {
-            int total = sc.nextInt();
-            String[] line = sc.nextLine().split(" ");
-            ArrayList<String> array1 = new ArrayList<>();
-            ArrayList<String> array2 = new ArrayList<>();
-            for (int j = 0; j < total; j++) {
-                if (j > total / 2)
-                    array2.add(line[j]);
-                else
-                    array1.add(line[j]);
 
-            }
-
-            System.out.println("#" + (i + 1) + " ");
-            for (int j = 0; j < total; j++) {
-                if(j%2==0)
-                array1.get(j);
-                else
-                array2.get(j);
-            }
-            System.out.println();
-        }
-
+            System.out.println("#"+i+" "+answer);
+        }      
     }
 }
