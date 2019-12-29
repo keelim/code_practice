5df3e3f45fc8c84ba56e37beb2461e135a126ccc
keelim <kimh00335@gmail.com>
2019-12-29 22:47:06 +0900

    2019 12 29

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
     int testCase;
     cin >> testCase;
-    for (int i = 0; i < testCase; i++)
+    for (auto i = 1; i <= testCase; i++)
     {
-        int total;
-        cin >> total;
-        int count;
-        cin >> count;
-        int *array = new int[total];
-        for (int i = 0; i < total; i++)
-        {
-            array[i] = -1; //초기화
-        }
-        for (auto i = 0; i < count; i++)
-        {
-            int target;
-            cin >> target;
-            array[target - 1] = 1;
-        }
-        cout << "#" << (i + 1) << " ";
-        for (auto i = 0; i < total; i++)
-        {
-            if (array[i] == -1)
-                cout << i + 1 << " ";
-        }
-        cout << "\n";
+        string answer = "";
+        string temp;
+        cin >> temp;
+        temp = temp.end;
+        int now = stoi(temp);
+        if (now % 2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
+
+        cout << "#" << i << " " << answer;
     }
 }
