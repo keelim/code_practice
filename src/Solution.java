ae6c308a2ea77034d4b25e01772084f5a958e7bc
keelim <kimh00335@gmail.com>
2019-12-30 22:01:49 +0900

    2019 12 30

--- src/Solution.java
+++ src/Solution.java
@@ -1,27 +1,42 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int testCase = sc.nextInt();
-        for (int i = 1; i <= testCase; i++) {
-            int num = sc.nextInt();
-            int count = 0;
-            for (int j = 0; j < num; j++) {
-                String[] temp = sc.next().toLowerCase().split("");
-                boolean flag = true;
-                for (int k = 0; k < temp.length / 2; k++) {
-                    if (temp[k].equals(temp[temp.length - 1 - k]))
-                        flag = true;
-                    else
-                        flag = false;
-                }
-                if (flag)
-                    count++;
-            }
+        // ArrayList로 구현
+		ArrayList<Boolean> primeList;
 
-            System.out.println("#" + i + " " + count);
-        }
+		// 사용자로부터의 콘솔 입력
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+
+		// n <= 1 일 때 종료
+		if(n <= 1) return;
+
+		// n+1만큼 할당
+		primeList = new ArrayList<Boolean>(n+1);
+		// 0번째와 1번째를 소수 아님으로 처리
+		primeList.add(false);
+		primeList.add(false);
+		// 2~ n 까지 소수로 설정
+		for(int i=2; i<=n; i++ )
+			primeList.add(i, true);
+
+		// 2 부터  ~ i*i <= n
+		// 각각의 배수들을 지워간다.
+		for(int i=2; (i*i)<=n; i++){
+			if(primeList.get(i)){
+				for(int j = i*2; j<=n; j+=i) primeList.set(j, false);
+				//i*i 미만은 이미 처리되었으므로 j의 시작값은 i*i로 최적화할 수 있다.
+			}
+		}
+		StringBuffer sb = new StringBuffer();
+		for(int i=0; i<=n; i++){
+			if(primeList.get(i) == true){
+				sb.append(i);
+				sb.append(" ");
+			}
+		}
+		System.out.println(sb.toString());
     }
 }
