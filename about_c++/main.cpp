45a93cfe1eb13f998e46ddc1ef46a7e3a7065ce5
keelim <kimh00335@gmail.com>
2020-01-05 20:25:38 +0900

    2020 01 05

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <set>
-#include <string>
 using namespace std;
 
-int main()
-{
-    int testCase;
-    cin >> testCase;
-    for (auto i = 1; i <= testCase; i++)
+int main(){
+    set<int> tempSet;
+    for (auto i = 0; i < 10; i++)
     {
-        set<int, greater<int>> col;
-        string line;
-        cin >> line;
-        for (auto i = 0; i < line.size(); i++)
-        {
-            col.insert(line[i]);
-        }
-        cout << "#" << i << " " << col.size() << "\n";
+        int temp;
+        cin>>temp;
+        temp%=42;
+        tempSet.insert(temp);
     }
+    cout<<tempSet.size()<<"\n";   
 }
