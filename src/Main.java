cc5ddfed5b4ac1cc0f46b6852d316a330d9e1e2c
keelim <kimh00335@gmail.com>
2020-01-09 18:01:52 +0900

    2020 01 09

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,24 @@
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {//4
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        int total = 0;
-        for (int i = 0; i < 9; i++) {
-            int temp=sc.nextInt();
-            arrayList.add(temp);
-            total+=temp;
-        }
-        int pointer1 = 0;
-        int pointer2 = pointer1+1;
-        while (total-(arrayList.get(pointer1)+arrayList.get(pointer2))!=100){
+        int E = sc.nextInt();
+        int S = sc.nextInt();
+        int M = sc.nextInt();
+
+        int pointer1 = 1, pointer2 = 1, pointer3 = 1, answer = 1;
+        while (!(pointer1 == E && pointer2 == S && pointer3 == M)) {
+            pointer1++;
             pointer2++;
-            if(pointer2 >= 9){
-                pointer1++;
-                pointer2 = pointer1+1;
-            }
+            pointer3++;
+            answer++;
+            if(pointer1>15) pointer1 = 1;
+            if(pointer2>28) pointer2 =1;
+            if(pointer3>19) pointer3 = 1;
         }
 
-        arrayList.remove(pointer1);
-        arrayList.remove(pointer2-1);
-
-        Collections.sort(arrayList);
-        for (int i = 0; i < 7; i++) {
-            System.out.println(arrayList.get(i));
-        }
+        System.out.println(answer);
     }
 }
