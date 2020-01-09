79d5e9cf7bb11149a9359ac83a6ac6227447d683
keelim <kimh00335@gmail.com>
2020-01-09 14:46:42 +0900

    2020 01 09

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,9 +1,25 @@
+#include <stdio.h>
 #include <iostream>
-#include <string>
 using namespace std;
 
-int main(){
-    string temp;
-    cin>>temp;
-    cout<<temp.length();
+int main(void)
+{
+    int n, v, a = 0;
+    int* arr = new int[100];
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        cin>>temp;
+        arr[i] = temp;
+    }
+    cin >> v;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] = v) //v랑 같으면 a++;
+            a++;
+    }
+    cout << a << "\n";
+
+    return 0;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,33 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) { 
-		Scanner sc = new Scanner(System.in);
-        String[] temp = sc.next().toLowerCase().split("");
-        int[] answer = new int[26];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i]=0;
+public class Main {//4
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int total = 0;
+        for (int i = 0; i < 9; i++) {
+            int temp=sc.nextInt();
+            arrayList.add(temp);
+            total+=temp;
+        }
+        int pointer1 = 0;
+        int pointer2 = pointer1+1;
+        while (total-(arrayList.get(pointer1)+arrayList.get(pointer2))!=100){
+            pointer2++;
+            if(pointer2 >= 9){
+                pointer1++;
+                pointer2 = pointer1+1;
+            }
         }
 
-        for (int i = 0; i < temp.length; i++) {
-            char s = temp[i].charAt(0);
-            int pointer = s-97;
-            answer[pointer]++;
-        }
+        arrayList.remove(pointer1);
+        arrayList.remove(pointer2-1);
 
-        for (int i = 0; i < answer.length; i++) {
-            System.out.print(answer[i]+" ");
+        Collections.sort(arrayList);
+        for (int i = 0; i < 7; i++) {
+            System.out.println(arrayList.get(i));
         }
-	}
+    }
 }
