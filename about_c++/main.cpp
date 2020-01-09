4bc3c0b090d85dec3a702b68c3653de933843f34
keelim <kimh00335@gmail.com>
2020-01-09 23:01:01 +0900

    2020 01 09

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 using namespace std;
 
 int main()
 {
-    
+    int a, b;
+    cin >> a >> b;
+    int gcd_a = a, gcd_b = b;
+    while (gcd_b > 0)
+    {
+        int temp = gcd_b;
+        gcd_b = gcd_a % gcd_b;
+        gcd_a = temp;
+    }
+
+    int lcm = (a * b) / gcd_a;
+    cout << gcd_a << "\n" << lcm;
 }
--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,22 @@ public class Main {// 4
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt(), b = sc.nextInt();
-        int gcd_a = a, gcd_b = b;
-        
-        while (gcd_b > 0) {
-            int temp = gcd_b;
-            gcd_b = gcd_a % gcd_b;
-            gcd_a = temp;
+
+        int testCase = sc.nextInt();
+        for (int i = 0; i < testCase; i++) {
+            int a = sc.nextInt(), b = sc.nextInt();
+            int gcd_a = a, gcd_b = b;
+
+            while (gcd_b > 0) {
+                int temp = gcd_b;
+                gcd_b = gcd_a % gcd_b;
+                gcd_a = temp;
+            }
+            int lcm = a * b / gcd_a;
+            System.out.println(lcm);
         }
-        int lcm = (a * b) / gcd_a;
-        System.out.println(gcd_a);
-        System.out.println(lcm);
+
+
     }
 
 
