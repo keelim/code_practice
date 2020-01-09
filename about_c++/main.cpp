007414fcee6efa8132f7cb7166a9da893279e9be
keelim <kimh00335@gmail.com>
2020-01-09 15:19:39 +0900

    2020 01 09

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,25 +1,32 @@
-#include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main(void)
-{
-    int n, v, a = 0;
-    int* arr = new int[100];
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
+int main () {
+    vector<int> vc;
+    int total=0;
+    for (auto i=0; i < 9; i++) {
         int temp;
-        cin>>temp;
-        arr[i] = temp;
+        cin >> temp;
+        vc.push_back (temp);
+        total+=temp;
     }
-    cin >> v;
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] = v) //v랑 같으면 a++;
-            a++;
+    int pointer1=0;
+    int pointer2=pointer1 + 1;
+    while (total - (vc.at (pointer1) + vc.at (pointer2)) != 100) {
+        pointer2++;
+        if (pointer2 >= 9) {
+            pointer1++;
+            pointer2=pointer1 + 1;
+        }
     }
-    cout << a << "\n";
 
-    return 0;
+    vc.erase (vc.begin () + pointer1);
+    vc.erase (vc.begin () + pointer2 - 1);
+
+    sort (vc.begin (), vc.end ());
+    for (auto i=0; i < 7; i++) {
+        cout << vc.at (i) << "\n";
+    }
 }
