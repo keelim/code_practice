5bb7bcb06f16a28388ea95ec88a9959d6fa6a2fc
keelim <kimh00335@gmail.com>
2020-01-09 22:48:02 +0900

    2020 01 09

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,32 +1,9 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int main () {
-    vector<int> vc;
-    int total=0;
-    for (auto i=0; i < 9; i++) {
-        int temp;
-        cin >> temp;
-        vc.push_back (temp);
-        total+=temp;
-    }
-    int pointer1=0;
-    int pointer2=pointer1 + 1;
-    while (total - (vc.at (pointer1) + vc.at (pointer2)) != 100) {
-        pointer2++;
-        if (pointer2 >= 9) {
-            pointer1++;
-            pointer2=pointer1 + 1;
-        }
-    }
-
-    vc.erase (vc.begin () + pointer1);
-    vc.erase (vc.begin () + pointer2 - 1);
-
-    sort (vc.begin (), vc.end ());
-    for (auto i=0; i < 7; i++) {
-        cout << vc.at (i) << "\n";
-    }
+int main()
+{
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,21 @@
 import java.util.Scanner;
 
-public class Main {//4
+public class Main {// 4
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int E = sc.nextInt();
-        int S = sc.nextInt();
-        int M = sc.nextInt();
-
-        int pointer1 = 1, pointer2 = 1, pointer3 = 1, answer = 1;
-        while (!(pointer1 == E && pointer2 == S && pointer3 == M)) {
-            pointer1++;
-            pointer2++;
-            pointer3++;
-            answer++;
-            if(pointer1>15) pointer1 = 1;
-            if(pointer2>28) pointer2 =1;
-            if(pointer3>19) pointer3 = 1;
+        int a = sc.nextInt(), b = sc.nextInt();
+        int gcd_a = a, gcd_b = b;
+        
+        while (gcd_b > 0) {
+            int temp = gcd_b;
+            gcd_b = gcd_a % gcd_b;
+            gcd_a = temp;
         }
-
-        System.out.println(answer);
+        int lcm = (a * b) / gcd_a;
+        System.out.println(gcd_a);
+        System.out.println(lcm);
     }
+
+
 }
