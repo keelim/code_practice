d935de9b57054f45dba0bb5949bf86e0ef2a1649
keelim <kimh00335@gmail.com>
2020-01-10 15:39:26 +0900

    2020 01 10

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,18 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
 using namespace std;
 
-int main()
-{
-    int a, b;
-    cin >> a >> b;
-    int gcd_a = a, gcd_b = b;
-    while (gcd_b > 0)
-    {
-        int temp = gcd_b;
-        gcd_b = gcd_a % gcd_b;
-        gcd_a = temp;
+const int MAX = 100000;
+bool visited[MAX];
+bool visited1[MAX];
+vector<int> graph[MAX];
+queue<int> Q;
+
+void dfs(int start) {
+    visited[start] = true;
+    cout << start << " ";
+    for (int y : graph[start]) {
+        if (!visited[y]) {
+            dfs(y);
+        }
+    }
+}
+
+void bfs(int start) {
+    visited1[start] = true;
+    Q.push(start);
+    while (!Q.empty()) {
+        int element = Q.front();
+        cout << element << " ";
+        Q.pop();
+        for (int y: graph[element]) {
+            if (!visited1[y]) {
+                visited1[y] = true;
+                Q.push(y);
+            }
+        }
+    }
+}
+
+int main() {
+    int node, edge, start;
+    cin >> node >> edge >> start;
+    for (int i = 0; i < node; i++) {
+        int head, tail;
+        cin >> head >> tail;
+        graph[head].push_back(tail);
+        graph[tail].push_back(head);
     }
 
-    int lcm = (a * b) / gcd_a;
-    cout << gcd_a << "\n" << lcm;
+    for (int i = 1; i <= node; i++) { //모든 노드 정렬
+        sort(graph[i].begin(), graph[i].end());
+    }
+
+    dfs(start);
+    cout << "\n";
+
+    bfs(start);
+    cout << "\n";
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,62 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {// 4
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int node = sc.nextInt(), edge = sc.nextInt(), start = sc.nextInt();
+        graph = new ArrayList[node + 1]; // 그래프 만들기 ArrayList으로 이루어진 배열
 
-        int testCase = sc.nextInt();
-        for (int i = 0; i < testCase; i++) {
-            int a = sc.nextInt(), b = sc.nextInt();
-            int gcd_a = a, gcd_b = b;
-
-            while (gcd_b > 0) {
-                int temp = gcd_b;
-                gcd_b = gcd_a % gcd_b;
-                gcd_a = temp;
-            }
-            int lcm = a * b / gcd_a;
-            System.out.println(lcm);
+        for (int i = 1; i < node + 1; i++) { //배열안에 객체 생성
+            graph[i] = new ArrayList<>();
         }
 
+        for (int i = 0; i < edge; i++) { //그래프 초기화
+            int head = sc.nextInt(), tail = sc.nextInt();
+
+            graph[head].add(tail);
+            graph[tail].add(head);
+        }
+        for (int i = 1; i < node + 1; i++) { //그래프 정렬
+            Collections.sort(graph[i]);
+        }
+        visited = new boolean[node + 1];
+        dfs(start);
+        System.out.println();
+
+        visited = new boolean[node + 1];
+        bfs(start);
+        System.out.println();
+    }
+
+    private static void dfs(int x) { //깊이 탐색
+        if (visited[x]) {
+            return;
+        }
+        visited[x] = true;
+        System.out.print(x + " ");
+        for (int y : graph[x]) {
+            if (!visited[y])
+                dfs(y);
+        }
 
     }
 
-
+    private static void bfs(int start) { //너비 탐색
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            System.out.print(x + " ");
+            for (int y : graph[x]) {
+                if (!visited[y]) {
+                    visited[y] = true;
+                    queue.add(y);
+                }
+            }
+        }
+    }
 }
