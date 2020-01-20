b346e5ba8237a8e01714079895ca83bd7f190852
keelim <kimh00335@gmail.com>
2020-01-20 21:32:04 +0900

    2020 01 20

--- about_c++/main.cpp
+++ about_c++/main.cpp
@@ -1,54 +1,41 @@
-#include<iostream>
-#include<queue>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
-#define MAX_SIZE 101
-int N, M;
-char map[MAX_SIZE][MAX_SIZE];
-bool visit[MAX_SIZE][MAX_SIZE];
-int dx[4] = {1, -1, 0, 0};    //동 서 남 북
-int dy[4] = {0, 0, -1, 1};    //동 서 남 북
 
-int bfs() {
-    queue<pair<pair<int, int>, int>> q;
-    q.push(make_pair(make_pair(0, 0), 1));    //첫째pair 위치, 둘째pair 움직인거리
-    visit[0][0] = 1;
+int answer = 0; // 값
+bool visited[101];
+vector<int> graph[101];
 
-    while (!q.empty()) {
-        int x = q.front().first.second;
-        int y = q.front().first.first;
-        int z = q.front().second;
+void dfs(int start){
+    if(visited[start]) return;
 
-        q.pop();
-
-        if (x == M - 1 && y == N - 1)
-            return z;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx < 0 || ny < 0 || nx >= M || ny >= N)
-                continue;
-            if (visit[ny][nx] == 1)
-                continue;
-            if (map[ny][nx] != '1')
-                continue;
-
-            q.push(make_pair(make_pair(ny, nx), z + 1));
-            visit[ny][nx] = 1;
-        }
+    visited[start] = true;
+    answer++;
+    for(int y : graph[start]){
+        if(!visited[y]) dfs(y);
     }
 }
 
-int main() {
+int main()
+{
+    int node, edge, start = 1;
+    cin >> node >> edge;
 
-    cin >> N >> M;
 
-    for (int i = 0; i < N; i++) {
-        cin >> map[i];
+    for (auto i = 0; i < edge; i++)
+    {
+        int head, tail;
+        cin >> head >> tail;
+        graph[head].push_back(tail);
+        graph[tail].push_back(head);
     }
-    cout << bfs();
 
-    return 0;
+    for (auto i = 1; i <= node; i++)
+    {
+        sort(graph[i].begin(), graph[i].end());
+    }
+    dfs(start);
+    cout<<answer-1;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,27 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-    static int first;
-    static int second;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static int[][] graph;
-    static boolean[][] visited;
-    static int wall;
-    static Queue<Pair<Integer, Integer>> queue;
+    static int location;
+    static int[] applicant;
+    static int master_dectector;
+    static int sub_detector;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        first = sc.nextInt();
-        second = sc.nextInt();
-
-        graph = new int[first][second];
-        visited = new boolean[first][second];
-
-        for (int i = first - 1; i >= 0; i--) {
-            String[] temp = sc.next().split("");
-            for (int j = 0; j < second; j++) {
-                graph[i][j] = Integer.parseInt(temp[j]);
-            }
-        } // 그래프 초기화
-
-        bfs();
-        System.out.println(wall);
-    }
-
-    static void bfs() {
-        queue = new LinkedList<>();
-
-        queue.add(new Pair(0, 0));
-
-        while (!queue.isEmpty()) {
-            Pair<Integer, Integer> element = queue.poll();
-            visited[element.left][element.right] = true;
-            for (int i = 0; i < 4; i++) {
-                int nx = element.left + dx[i];
-                int ny = element.right + dy[i];
-
-                if (nx < 0 || nx >= first || ny < 0 || ny >= second) continue;
-                if (visited[nx][ny]) continue;
-
-                queue.add(new Pair<>(nx, ny));
-            }
+        location = sc.nextInt();
+        applicant = new int[location];
+        for (int i = 0; i < location; i++) {
+            applicant[i] = sc.nextInt();
         }
-    }
-
-    static class Pair<L, R> {
-        L left;
-        R right;
-
-        public Pair(L left, R right) {
-            this.left = left;
-            this.right = right;
+        master_dectector = sc.nextInt();
+        sub_detector = sc.nextInt();
+        long answer = location;
+        for (int i = 0; i < location; i++) {
+            int temp = applicant[i];
+            temp -= master_dectector;
+            answer+=(temp/sub_detector);
+            if(temp%sub_detector>0) answer++;
         }
+        System.out.println(answer);
     }
 }
