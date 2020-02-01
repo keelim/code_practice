28db09075fc75ab3a841fbde4d5c69f7f43071af
keelim <kimh00335@gmail.com>
2020-02-01 12:02:20 +0900

    20202001

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+    static int[] memo = new int[10];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Arrays.fill(memo, -1);
         int first = sc.nextInt();
         int second = sc.nextInt();
+        memo[0] = 1;
+        memo[1] = 1;
+        int answer = factorial(first)/(factorial(second)*factorial(first-second));
+        System.out.println(answer);
+    }
 
-        if(first - second>0){
-            System.out.println(">");
-        } else if(first-second == 0){
-            System.out.println("==");
-        } else{
-            System.out.println("<");
-        }
+    static int factorial(int n){
+        if(memo[n]!=-1) return memo[n];
+        memo[n] = memo[n-1]+memo[n-2];
+        return memo[n];
     }
 }
