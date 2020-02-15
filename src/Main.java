397fcd32ef7d28f80825473067911942a5a10081
h0033 <kimh00335@gmail.com>
2020-02-15 22:07:46 +0900

    2020 02 15

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,29 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    
+    public static int cnt = 0;
+    public static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-                
-        int answer = fibo(num);
-        System.out.println(answer);
+        int n = sc.nextInt();
+
+        hanoi(n, 1, 2, 3);
+        sb.insert(0, cnt + "\n");
+        
+        System.out.println(sb);
+        sc.close();
     }
 
-    static int fibo(int num) {
-        if(num==0) return 0;
-        if(num==1) return 1;
-
-        return fibo(num-1)+fibo(num-2);
+    public static void hanoi(int num, int a, int b, int c) {
+        if (num == 1) {
+            sb.append(a + " " + c + "\n");
+            cnt++;
+        } else {
+            hanoi(num - 1, a, c, b);// n-1개의 원반을 A에서 B로 이동
+            sb.append(a + " " + c + "\n");
+            cnt++;
+            hanoi(num - 1, b, a, c);// n-1개의 원반을 B에서 C로
+        }
     }
 }
