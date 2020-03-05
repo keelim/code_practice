cab0fb2dfd479d638f987ef8b991f7eb829dbe65
h0033 <kimh00335@gmail.com>
2020-03-05 20:26:03 +0900

    2020 03 05

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,18 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-    public static int cnt = 0;
-    public static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int num = sc.nextInt();
 
-        hanoi(n, 1, 2, 3);
-        sb.insert(0, cnt + "\n");
-        
-        System.out.println(sb);
-        sc.close();
-    }
-
-    public static void hanoi(int num, int a, int b, int c) {
-        if (num == 1) {
-            sb.append(a + " " + c + "\n");
-            cnt++;
-        } else {
-            hanoi(num - 1, a, c, b);// n-1개의 원반을 A에서 B로 이동
-            sb.append(a + " " + c + "\n");
-            cnt++;
-            hanoi(num - 1, b, a, c);// n-1개의 원반을 B에서 C로
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < num; i++) {
+            String s = sc.next();
+            arrayList.add(s);
         }
+
+        Collections.sort(arrayList);
     }
 }
