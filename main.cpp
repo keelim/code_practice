52752d91c9ddcf32247d715ea96b1d8ee5b8aec2
unknown <201503069@o.cnu.ac.kr>
2020-04-21 22:58:30 +0900

    2020 04 21

--- main.cpp
+++ main.cpp
@@ -1,63 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-vector<int> vc;
-int total = 0;
-
-int main()
+int solution(int n)
 {
-    
-    for (int i = 0; i < 9; i++)
+    int sum = n;
+    while (n)
     {
-        int temp;
-        cin >> temp;
-        vc.push_back(temp);
-        total += temp;
+        sum += n % 10;
+        n /= 10;
     }
-    int pointer1 = 0;
-    int pointer2 = pointer1 + 1;
-    while (total - (vc.at(pointer1) + vc.at(pointer2)) != 100)
-    {
-        pointer2++;
-        if (pointer2 >= 9)
-        {
-            pointer1++;
-            pointer2 = pointer1 + 1;
-        }
-    }
+    return sum;
+}
 
-    vc.erase(vc.begin() + pointer1);
-    vc.erase(vc.begin() + pointer2 - 1);
-
-    sort(vc.begin(), vc.end());
-    for (int i = 0; i < 7; i++)
-    {
-        cout << vc[i] << "\n";
-    }
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    int N;
+    cin >> N;
 
-    int temp;
-    vector<int> vc;
-    cin >> temp;
-    for (int i = 1; i <= temp; i++)
+    for (int i = 1; i < N; i++)
     {
-        vc.push_back(i);
-    }
-
-    sort(vc.begin(), vc.end());
-
-    do
-    {
-        for (int i = 0; i < vc.size(); i++)
+        int sum = solution(i);
+        if (sum == N)
         {
-            cout << vc[i] << " ";
+            cout << i << "\n";
+            return 0;
         }
-        cout << "\n";
-    } while (next_permutation(vc.begin(), vc.end()));
+    }
+    cout << 0 << "\n";
 }
