0b80641e40bf0957938c0afc40d11f5e4228a8f0
unknown <201503069@o.cnu.ac.kr>
2020-04-21 22:48:34 +0900

    2020 04 21

--- main.cpp
+++ main.cpp
@@ -1,100 +1,39 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int n, m, ans;
-int map[501][501];
-bool visited[501][501] = { //memset 안써도 된다.
-    false,
-};
+vector<int> vc;
+int total = 0;
 
-int dx[4] = {0, 0, 1, -1};
-int dy[4] = {1, -1, 0, 0};
-
-int exception_case[4][4][2] = {
-    {{0, 0}, {0, -1}, {0, 1}, {-1, 0}},
-    {{0, 0}, {-1, 0}, {1, 0}, {0, 1}},
-    {{0, 0}, {0, -1}, {0, 1}, {1, 0}},
-    {{0, 0}, {-1, 0}, {1, 0}, {0, -1}}};
-
-void dfs(int x, int y, int cnt, int sum)
+int main()
 {
-    // 테트로미노를 만든 경우
-    if (cnt == 4)
+    
+    for (int i = 0; i < 9; i++)
     {
-        ans = max(ans, sum);
-        return;
+        int temp;
+        cin >> temp;
+        vc.push_back(temp);
+        total += temp;
     }
-
-    for (int i = 0; i < 4; i++)
+    int pointer1 = 0;
+    int pointer2 = pointer1 + 1;
+    while (total - (vc.at(pointer1) + vc.at(pointer2)) != 100)
     {
-        int nx = x + dx[i];
-        int ny = y + dy[i];
-
-        if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            
-        if (!visited[nx][ny])
+        pointer2++;
+        if (pointer2 >= 9)
         {
-            visited[nx][ny] = true;
-            dfs(nx, ny, cnt + 1, sum + map[nx][ny]);
-            visited[nx][ny] = false;
-        }
-    }
-}
-
-// 'ㅗ' 의 경우 예외처리
-// 4방향 x 정사각형 4개
-void getExceptionCase(int x, int y)
-{
-    for (int i = 0; i < 4; i++)
-    {
-        bool flag = true;
-        int sum = 0;
-
-        for (int j = 0; j < 4; j++)
-        {
-            int nx = x + exception_case[i][j][0];
-            int ny = y + exception_case[i][j][1];
-
-            if (nx < 0 || ny < 0 || nx >= n || ny >= m)
-            {
-                flag = false;
-                break;
-            }
-
-            sum += map[nx][ny];
-        }
-
-        if (flag)
-            ans = max(ans, sum);
-    }
-}
-
-int main(int argc, const char *argv[])
-{
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> map[i][j];
-        }
-    } //입력
-    ans = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            visited[i][j] = true;
-            dfs(i, j, 1, map[i][j]);
-            visited[i][j] = false;
-
-            // 'ㅗ' 의 경우 예외처리
-            getExceptionCase(i, j); //새로운 dfs
+            pointer1++;
+            pointer2 = pointer1 + 1;
         }
     }
 
-    cout << ans << "\n";
+    vc.erase(vc.begin() + pointer1);
+    vc.erase(vc.begin() + pointer2 - 1);
+
+    sort(vc.begin(), vc.end());
+    for (int i = 0; i < 7; i++)
+    {
+        cout << vc[i] << "\n";
+    }
 }
