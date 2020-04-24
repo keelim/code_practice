e15d1d43f9c8d828e829beb064f2f7bca50d6000
unknown <201503069@o.cnu.ac.kr>
2020-04-24 22:51:32 +0900

    2020 04 24

--- main.cpp
+++ main.cpp
@@ -1,183 +1,67 @@
 #include <iostream>
-#include <vector>
-#include <string>
+#include <algorithm>
 
 using namespace std;
 
-int K;
+int N, M, H, Answer;
+int MAP[11][30];
+bool Visit[11][30];
 
-vector<int> Wheel[4];
-vector<pair<int, int>> vc;
 
-
-int Reverse_Direction(int d)
+bool Ladder_Game()
 {
-    if (d == 1)
-        return -1;
-    else
-        return 1;
+    for (int i = 1; i <= N; i++)
+    {
+        int Current_Num = i;
+        for (int j = 1; j <= H; j++)
+        {
+            if (Visit[Current_Num][j] == true)
+                Current_Num = Current_Num + 1;
+            else if (Visit[Current_Num - 1][j] == true)
+                Current_Num = Current_Num - 1;
+        }
+
+        if (Current_Num != i)
+            return false;
+    }
+    return true;
 }
 
-void Actual_Turning(int n, int d)
+void Select_Line(int Idx, int Cnt)
 {
-    if (d == 1)
-    {
-        int Tmp = Wheel[n].at(7);
-        for (int i = 7; i > 0; i--)
-        {
-            Wheel[n].at(i) = Wheel[n].at(i - 1);
-        }
-        Wheel[n].at(0) = Tmp;
-    }
-    else if (d == -1)
-    {
-        int Tmp = Wheel[n].at(0);
-        for (int i = 0; i < 7; i++)
-        {
-            Wheel[n].at(i) = Wheel[n].at(i + 1);
-        }
-        Wheel[n].at(7) = Tmp;
-    }
-}
+    if (Cnt >= 4)
+        return;
 
-void Turn_Wheel(int n, int d)
-{
-    int nd = Reverse_Direction(d);
-    if (n == 0)
+    if (Ladder_Game() == true)
     {
-        if (Wheel[n].at(2) != Wheel[n + 1].at(6))
-        {
-            if (Wheel[n + 1].at(2) != Wheel[n + 2].at(6))
-            {
-                if (Wheel[n + 2].at(2) != Wheel[n + 3].at(6))
-                {
-                    Actual_Turning(n, d);
-                    Actual_Turning(n + 1, nd);
-                    Actual_Turning(n + 2, d);
-                    Actual_Turning(n + 3, nd);
-                }
-                else
-                {
-                    Actual_Turning(n, d);
-                    Actual_Turning(n + 1, nd);
-                    Actual_Turning(n + 2, d);
-                }
-            }
-            else
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n + 1, nd);
-            }
-        }
-        else
-        {
-            Actual_Turning(n, d);
-        }
+        Answer = min(Answer, Cnt);
+        return;
     }
-    else if (n == 1)
-    {
-        if (Wheel[n].at(6) != Wheel[n - 1].at(2))
-        {
-            Actual_Turning(n - 1, nd);
-        }
 
-        if (Wheel[n].at(2) != Wheel[n + 1].at(6))
-        {
-            if (Wheel[n + 1].at(2) != Wheel[n + 2].at(6))
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n + 1, nd);
-                Actual_Turning(n + 2, d);
-            }
-            else
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n + 1, nd);
-            }
-        }
-        else
-        {
-            Actual_Turning(n, d);
-        }
-    }
-    else if (n == 2)
+    for (int i = Idx; i <= H; i++)
     {
-        if (Wheel[n].at(2) != Wheel[n + 1].at(6))
+        for (int j = 1; j < N; j++)
         {
-            Actual_Turning(n + 1, nd);
-        }
+            if (Visit[j][i] == true)
+                continue;
+            if (Visit[j - 1][i] == true)
+                continue;
+            if (Visit[j + 1][i] == true)
+                continue;
 
-        if (Wheel[n].at(6) != Wheel[n - 1].at(2))
-        {
-            if (Wheel[n - 1].at(6) != Wheel[n - 2].at(2))
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n - 1, nd);
-                Actual_Turning(n - 2, d);
-            }
-            else
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n - 1, nd);
-            }
-        }
-        else
-        {
-            Actual_Turning(n, d);
-        }
-    }
-    else if (n == 3)
-    {
-        if (Wheel[n].at(6) != Wheel[n - 1].at(2))
-        {
-            if (Wheel[n - 1].at(6) != Wheel[n - 2].at(2))
-            {
-                if (Wheel[n - 2].at(6) != Wheel[n - 3].at(2))
-                {
-                    Actual_Turning(n, d);
-                    Actual_Turning(n - 1, nd);
-                    Actual_Turning(n - 2, d);
-                    Actual_Turning(n - 3, nd);
-                }
-                else
-                {
-                    Actual_Turning(n, d);
-                    Actual_Turning(n - 1, nd);
-                    Actual_Turning(n - 2, d);
-                }
-            }
-            else
-            {
-                Actual_Turning(n, d);
-                Actual_Turning(n - 1, nd);
-            }
-        }
-        else
-        {
-            Actual_Turning(n, d);
+            Visit[j][i] = true;
+            Select_Line(i, Cnt + 1);
+            Visit[j][i] = false;
         }
     }
 }
 
 void Solution()
 {
-    for (int i = 0; i < vc.size(); i++)
-    {
-        int N = vc[i].first;
-        int D = vc[i].second;
+    Select_Line(1, 0);
 
-        Turn_Wheel(N, D);
-    }
-
-    int Answer = 0;
-    if (Wheel[0].at(0) == 1)
-        Answer = Answer + 1;
-    if (Wheel[1].at(0) == 1)
-        Answer = Answer + 2;
-    if (Wheel[2].at(0) == 1)
-        Answer = Answer + 4;
-    if (Wheel[3].at(0) == 1)
-        Answer = Answer + 8;
+    if (Answer == 9999999)
+        Answer = -1;
 
     cout << Answer << "\n";
 }
@@ -188,26 +72,14 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    for (int i = 0; i < 4; i++)
+    Answer = 9999999;
+    cin >> N >> M >> H;
+    for (int i = 0; i < M; i++)
     {
-        string Inp;
-        cin >> Inp;
-        for (int j = 0; j < Inp.length(); j++)
-        {
-            int Tmp = Inp[j] - '0';
-            Wheel[i].push_back(Tmp);
-        }
-    }
-    // 1은 시계방향 -1은 반시계방향
-    cin >> K;
-    for (int i = 0; i < K; i++)
-    {
-        int Num, Dir;
-        cin >> Num >> Dir;
-        Num = Num - 1;
-        vc.push_back({Num, Dir});
+        int a, b;
+        cin >> a >> b;
+        Visit[b][a] = true;
     }
 
     Solution();
-
 }
