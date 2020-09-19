f703d01a183714a7b984d747ba8af7ed3a97a365
keelim <kimh00335@gmail.com>
2020-09-19 15:59:58 +0900

    2020 09 19

--- main.cpp
+++ main.cpp
@@ -1,47 +1,74 @@
 #include <iostream>
-#include <tuple>
+#include <vector>
 #include <queue>
+#include <tuple>
+#include <algorithm>
 
 using namespace std;
 
-int time = 0;
-bool visited[200001][2];
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+int n, m;
+char map[251][251];
+bool visited[251][251];
 
-int solve(int cPosition, int bPosition) {
-    queue <pair<int, int>> q;
-    q.push(make_pair(bPosition, 0));
+pair<int, int> bfs(int a, int b) {
+    queue<pair<int, int>> q;
+    q.push(make_pair(a, b));
+    visited[a][b] = 1;
+    int c = 0;
+    int d = 0;
+    while (!q.empty()) {
+        int x, y;
+        tie(x, y) = q.front();
+        q.pop();
 
-    while (1) {
-        cPosition += time;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-        if (cPosition > 200000) return -1;
-        if (visited[cPosition][time % 2]) return time;
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
 
-        for (int i = 0, size = q.size(); i < size; i++) {
-            int currentPosition = q.front().first;
-            int newTime = (q.front().second + 1) % 2;
-            int newPosition;
-            q.pop();
-
-            newPosition = currentPosition - 1;
-            if (newPosition >= 0 && !visited[newPosition][newTime]) {
-                visited[newPosition][newTime] = true;
-                q.push(make_pair(newPosition, newTime));
-            }
-
-            newPosition = currentPosition + 1;
-            if (newPosition < 200001 && !visited[newPosition][newTime]) {
-                visited[newPosition][newTime] = true;
-                q.push(make_pair(newPosition, newTime));
-            }
-
-            newPosition = currentPosition * 2;
-            if (newPosition < 200001 && !visited[newPosition][newTime]) {
-                visited[newPosition][newTime] = true;
-                q.push(make_pair(newPosition, newTime));
+            if (!visited[nx][ny] && map[nx][ny] != '#') {
+                if (map[nx][ny] == 'v') {
+                    d++;
+                } else if (map[nx][ny] == 'k') {
+                    c++;
+                }
+                q.push(make_pair(nx, ny));
+                visited[nx][ny] = 1;
             }
         }
-
-        time++;
     }
+
+    if(c>d){
+        d=0;
+    } else{
+        c=0;
+    }
+
+    return make_pair(c, d);
+}
+
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        string s;
+        cin >> s;
+        for (int j = 0; j < m; j++) {
+            map[i][j] = s[j];
+        }
+    } //input
+    pair<int, int> answer = {0, 0};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            pair<int, int> a = bfs(i, j);
+            answer.first += a.first;
+            answer.second += a.second;
+        }
+    }
+
+    cout << answer.first << ' ' << answer.second << '\n';
+    return 0;
 }
