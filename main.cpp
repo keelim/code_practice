dbef614970f69b95f2523c154bfbd8cb17c6f5c8
keelim <kimh00335@gmail.com>
2020-10-23 11:42:27 +0900

    섬 연결하기

--- main.cpp
+++ main.cpp
@@ -1,53 +1,48 @@
-#include <iostream>
+#include <string>
 #include <vector>
 #include <queue>
+#include <tuple>
+
 
 using namespace std;
 
-int visited[10001];
-vector<pair<int, int>> map[10001];
+bool visited[105];
+vector<pair<int, int>> map[105];
 
-/* 프링 알고리즘
-   우선순위 큐를 이용하여 방문할 수 있는 정점중 가중치가 가장 낮은 정점으로 이동
-   프림은 ElogV 의 시간복잡도
-*/
+int solution (int n, vector<vector<int>> costs) {
+	int answer=0;
+	for (int i=0; i < costs.size (); i++) {
+		int a=costs[i][0];
+		int b=costs[i][1];
+		int cost=costs[i][2];
 
-int prim () {
-	long long ans=0;
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-	// first는 가중치 second 는 정점 minheap 
-	pq.push ({ 0,1 });
+		map[a].push_back ({ b, cost });
+		map[b].push_back ({ a, cost });
+	}
 
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+	for (int i=0; i < map[0].size (); i++) {
+		int x=map[0][i].first;
+		int cost=map[0][i].second;
+		pq.push ({ cost, x });
+	}
+	visited[0]=true;
 	while (!pq.empty ()) {
-		pair<int, int> x=pq.top ();
+		int cost, x;
+		tie (cost, x)=pq.top ();
 		pq.pop ();
 
-		if (visited[x.second]) continue;
-		visited[x.second]=1;
+		if (!visited[x]) {
+			visited[x]=true;
+			answer+=cost;
 
-		ans+=x.first;
-		for (int i=0; i < map[x.second].size (); i++) {
-			//현재 정점에서 이동 할 수 있는 방문하지 않은 정점 푸쉬 
-			if (!visited[map[x.second][i].second]) {
-				pq.push (map[x.second][i]);
+			for (int i=0; i < map[x].size (); i++) {
+				int nx, ncost;
+				tie (nx, ncost)=map[x][i];
+				if (!visited[nx])
+					pq.push ({ ncost, nx });
 			}
 		}
 	}
-	return ans;
-}
-
-int main () {
-	int n, m;
-	cin >> n >> m;
-
-	for (int i=0; i < m; i++) {
-		int a, b, c;
-		cin >> a >> b >> c;
-		map[a].push_back ({ c, b });
-		map[b].push_back ({ c, a });
-	}
-
-	long long result=prim ();
-	cout << result << '\n';
-	return 0;
+	return answer;
 }
